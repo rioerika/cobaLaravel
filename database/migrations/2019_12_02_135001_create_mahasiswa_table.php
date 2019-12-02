56b8e51b7a7056d1fb710d26d622b4b5a3f84bc8
Rio Erika <rio_173040041@mail.ac.id>
2019-12-02 21:00:51 +0700

    Membuat struktur table mahasiswa dan students

---
+++ database/migrations/2019_12_02_135001_create_mahasiswa_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('nama');
+            $table->char('nrp');
+            $table->string('email');
+            $table->string('jurusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
